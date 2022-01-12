db2bc6efae1535c8b0e4e1561a1a824295965b92
omarabbasi <omarkabbasi@outlook.com>
2022-01-12 14:39:46 -0800

    commit tree 6871cb028fe4803bb216593a233ee8529729942e

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
+  function superbowlWin(){
+   return ('2015')
+  }
+    let recordList = record.find(record=>{
+        return (record.year.superbowlWin) 
+    })
+    
+console.log(recordList)
